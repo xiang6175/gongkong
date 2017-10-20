6d786f020cb9374bc8a041c512a2709ca9e7ce46
Administrator <Administrator@LUJ7EQP5GS6IJZS>
2017-10-20 13:20:58 +0800

    添加项目文件。

---
+++ gongkong/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace gongkong
+{
+    partial class control
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-2, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "C型臂升";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, -1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 31);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "C型臂降";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(166, -1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 31);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "C型臂左倾";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(250, -1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 31);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "C型臂右倾";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(334, -1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(78, 31);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "射线管左移";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(418, -1);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(78, 31);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "射线管右移";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(772, 263);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(146, 60);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "确定";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(-2, 263);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(146, 60);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "保存";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(502, -1);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(78, 31);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "平板左移";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(586, -1);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(78, 31);
+            this.button10.TabIndex = 0;
+            this.button10.Text = "平板右移";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(670, -1);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(78, 31);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "小车前进";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(754, -1);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(78, 31);
+            this.button12.TabIndex = 0;
+            this.button12.Text = "小车后退";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(-2, 36);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(78, 31);
+            this.button13.TabIndex = 0;
+            this.button13.Text = "小车左移";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(82, 36);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(78, 31);
+            this.button14.TabIndex = 0;
+            this.button14.Text = "小车右移";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(166, 36);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(78, 31);
+            this.button15.TabIndex = 0;
+            this.button15.Text = "平台左转";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(250, 36);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(78, 31);
+            this.button16.TabIndex = 0;
+            this.button16.Text = "平台右转";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // control
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 321);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "control";
+            this.Text = "控制";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+    }
+}
+
